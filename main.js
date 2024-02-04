10a714b8fcb4e58e01406454fa1fd92a46630800
Wendel <wendellduos@hotmail.com>
2024-02-04 16:44:38 -0300

    added animation in subtitle

--- main.js
+++ main.js
@@ -12,3 +12,45 @@ navItems.forEach((e) => {
     }
   });
 });
+
+// header's 'p' animation
+const subtitle = document.getElementById("header-subtitle");
+const subtitleText = "Is anybody out there?";
+const subtitleArray = [];
+
+for (i = 0; i < subtitleText.length; i++) {
+  subtitleArray.push(subtitleText[i]);
+}
+
+subtitleArray.forEach((letter) => {
+  subtitle.innerHTML += `<p class="subtitle">${letter}</p>`;
+});
+
+const subtitleLetters = document.querySelectorAll(".subtitle");
+
+setInterval(() => {
+  const animSpeed = 25;
+  let subCount = 1;
+
+  subtitleLetters.forEach((letter) => {
+    setTimeout(function () {
+      letter.style.color = "var(--dark-dull)";
+      letter.style.marginLeft = ".5px";
+    }, subCount * animSpeed);
+
+    subCount++;
+  });
+
+  let subCountLn = 1;
+
+  setTimeout(() => {
+    subtitleLetters.forEach((letter) => {
+      setTimeout(function () {
+        letter.style.color = "var(--dark)";
+        letter.style.marginLeft = "0px";
+      }, subCountLn * animSpeed);
+
+      subCountLn++;
+    });
+  }, subtitleText.length * animSpeed);
+}, 7000);
